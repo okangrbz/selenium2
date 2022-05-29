7708648d95db4f7f26741c221af93a9f194c379e
Okan Gurbuz <okangurbuz06@gmail.com>
2022-05-29 22:04:55 +0200

    day18

--- IdeaProjects/untitled1com.TestNG_Bach59/src/test/java/tests/day17_pom/C04_PageClassKullanimi.java
+++ IdeaProjects/untitled1com.TestNG_Bach59/src/test/java/tests/day17_pom/C04_PageClassKullanimi.java
@@ -1,5 +1,12 @@
 package tests.day17_pom;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
 public class C04_PageClassKullanimi {
 
     /*
@@ -12,4 +19,38 @@ public class C04_PageClassKullanimi {
  Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
      */
 
+    @Test
+    public void positiveLoginTest() {
+
+
+        // https://www.hotelmycamp.com/ adresine git
+
+        Driver.getDriver().get("https://www.hotelmycamp.com/");
+
+        // login butonuna bas
+
+        Driver.getDriver().findElement(By.xpath("(//*[@class='nav-link'])[7]")).click();
+
+
+        Actions action=new Actions(Driver.getDriver());
+        // test data username: manager ,
+        Driver.getDriver().findElement(By.xpath("//input[@name='UserName']")).sendKeys("manager");
+        action.sendKeys(Keys.TAB);
+
+
+
+        // test data password : Manager1!
+        Driver.getDriver().findElement(By.xpath("//input[@name='Password']")).sendKeys("Manager1!");
+        Driver.getDriver().findElement(By.xpath("//input[@value='Log in']")).click();
+
+
+        // Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
+        Assert.assertTrue(Driver.getDriver().findElement(By.className("page-content-wrapper")).isDisplayed());
+
+
+
+
+
+
+    }
 }
--- IdeaProjects/untitled1com.TestNG_Bach59/src/test/java/utilities/Driver.java
+++ IdeaProjects/untitled1com.TestNG_Bach59/src/test/java/utilities/Driver.java
@@ -3,6 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -19,10 +22,42 @@ public class Driver {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            switch (ConfigReader.getProperty("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+                    break;
+
+                case "headless-chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+                    break;
+
+                case "firefox":
+
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+                    break;
+
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+
+
+            }
+
+
         }
 
         return driver;
@@ -31,7 +66,7 @@ public class Driver {
     public static void closeDriver() {
         if (driver != null) {//diver a bir deger atanmis
             driver.quit();
-            driver=null;
+            driver = null;
 
         }
     }
